6f57ece6a367c6ca765a820db2b89546ef764f80
Alfredo <alfredo@dvlop.mx>
2016-04-22 18:45:09 -0500

    inicio menu hecho y con fondo, carpetas nuevas agregadas

---
+++ Assets/scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log ("Loading level: " + name);
+		SceneManager.LoadScene (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested!");
+		Application.Quit ();
+	}
+}
